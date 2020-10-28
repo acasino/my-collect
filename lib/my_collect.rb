db13baccb321362fcbdb37b666396d4d33e46309
Andrew <andrew.casin@gmail.com>
2020-10-28 01:09:01 -0500

    commit tree 86e808ec873a8bddd318ec7c2d4ca772fb2f17b8

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+#my_collect method behaves just like the real #collect method. It should take in an 
+#argument of a collection, iterate over that collection using a while loop, and 
+#execute the code in the block you call it with for each element in the collection 
+#(use the yield keyword). It should return the modified collection.
+def my_collect(array)
+    i = 0
+    collection = []
+    while i < array.length
+        collection << yield(array[i])
+        i += 1
+    end
+    collection
+end
+
 
 
